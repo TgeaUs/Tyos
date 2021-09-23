64b412cd0977101168f751e75a4dcf64861397c8
tgeaus <tgeaus@gmail.com>
2021-09-23 04:23:20 -0700

    Update

--- build.sh
+++ build.sh
@@ -10,10 +10,10 @@ dd if=mbr.bin of=hd60M.img bs=512 count=1 conv=notrunc
 dd if=loader.bin of=hd60M.img bs=512 count=4 seek=2 conv=notrunc
 
 
-# echo "编译内核..."
-# nasm -f elf -o lib/print.o lib/print.asm
-# gcc -m32 -I ./lib/ -c -o lib/kernel/main.o lib/kernel/main.c
-# ld -m elf_i386 -Ttext 0xc0001500 -e main -o kernel.bin lib/kernel/main.o lib/print.o
+echo "编译内核..."
+nasm -f elf -o lib/print.o lib/print.asm
+gcc -m32 -I ./lib/ -c -o lib/main.o lib/main.c
+ld -m elf_i386 -Ttext 0xc0001500 -e main -o kernel.bin lib/main.o lib/print.o
 
 
-# dd if=kernel.bin of=hd60M.img bs=512 count=200 seek=9 conv=notrunc
+dd if=kernel.bin of=hd60M.img bs=512 count=200 seek=9 conv=notrunc
