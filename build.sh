7073aa25881bf8bafa8897823222c02e3e880d7e
tgeaus <tgeaus@gmail.com>
2021-09-23 04:14:28 -0700

    update

--- build.sh
+++ build.sh
@@ -9,3 +9,11 @@ echo "Writing mbr and loader to disk..."
 dd if=mbr.bin of=hd60M.img bs=512 count=1 conv=notrunc
 dd if=loader.bin of=hd60M.img bs=512 count=4 seek=2 conv=notrunc
 
+
+# echo "编译内核..."
+# nasm -f elf -o lib/print.o lib/print.asm
+# gcc -m32 -I ./lib/ -c -o lib/kernel/main.o lib/kernel/main.c
+# ld -m elf_i386 -Ttext 0xc0001500 -e main -o kernel.bin lib/kernel/main.o lib/print.o
+
+
+# dd if=kernel.bin of=hd60M.img bs=512 count=200 seek=9 conv=notrunc
--- loader.asm
+++ loader.asm
@@ -3,10 +3,8 @@
 section loader vstart=LOADER_BASE_ADDR
 LOADER_STACK_TOP equ LOADER_BASE_ADDR
 
-jmp loader_start
-
 ; 这里其实就是GDT的起始地址，第一个描述符为空
-GDT_BASE: dd 0x00000000 
+GDT_BASE: dd 0x00000000
           dd 0x00000000
 
 ; 代码段描述符，一个dd为4字节，段描述符为8字节，上面为低4字节
@@ -28,127 +26,278 @@ SELECTOR_CODE equ (0x0001 << 3) + TI_GDT + RPL0
 SELECTOR_DATA equ (0x0002 << 3) + TI_GDT + RPL0
 SELECTOR_VIDEO equ (0x0003 << 3) + TI_GDT + RPL0
 
+; 内存大小，单位字节，此处的内存地址是0xb00
+total_memory_bytes dd 0
+
 gdt_ptr dw GDT_LIMIT
         dd GDT_BASE
 
-loadermsg db '2 loader in real.'
+ards_buf times 244 db 0
+ards_nr dw 0
 
 loader_start: 
 
-        ; 调用10h号中断显示字符串
-        mov sp, LOADER_BASE_ADDR
-        mov bp, loadermsg
-        ; 字符串长度
-        mov cx, 17
-        ; 子功能号以及显示方式
-        mov ax, 0x1301
-        ; 页号:0, 蓝底粉红字
-        mov bx, 0x001f
-        mov dx, 0x1800
-        int 0x10
+    xor ebx, ebx
+    mov edx, 0x534d4150
+    mov di, ards_buf
 
-        ; 打开A20地址线
-        in al, 0x92
-        or al, 00000010B
-        out 0x92, al
+.e820_mem_get_loop:
+    mov eax, 0x0000e820
+    mov ecx, 20
+    int 0x15
+    
+    jc .e820_mem_get_failed
+    
+    add di, cx
+    inc word [ards_nr]
+    cmp ebx, 0
+    jnz .e820_mem_get_loop
 
-        ; 加载gdt
-        lgdt [gdt_ptr]
+    mov cx, [ards_nr]
+    mov ebx, ards_buf
+    xor edx, edx
 
-        ; cr0第0位置1
-        mov eax, cr0
-        or eax, 0x00000001
-        mov cr0, eax
+.find_max_mem_area:
+    mov eax, [ebx]
+    add eax, [ebx + 8]
+    add ebx, 20
+    cmp edx, eax
+    jge .next_ards
+    mov edx, eax
 
-        ; 刷新流水线
-        jmp dword SELECTOR_CODE:p_mode_start
+.next_ards:
+    loop .find_max_mem_area
+    jmp .mem_get_ok
+
+.e820_mem_get_failed:
+    mov byte [gs:0], 'f'
+    mov byte [gs:2], 'a'
+    mov byte [gs:4], 'i'
+    mov byte [gs:6], 'l'
+    mov byte [gs:8], 'e'
+    mov byte [gs:10], 'd'
+    ; 内存检测失败，不再继续向下执行
+    jmp $
+
+.mem_get_ok:
+    mov [total_memory_bytes], edx
+
+    ; 开始进入保护模式
+    ; 打开A20地址线
+    in al, 0x92
+    or al, 00000010B
+    out 0x92, al
+
+    ; 加载gdt
+    lgdt [gdt_ptr]
+
+    ; cr0第0位置1
+    mov eax, cr0
+    or eax, 0x00000001
+    mov cr0, eax
+
+    ; 刷新流水线
+    jmp dword SELECTOR_CODE:p_mode_start
 
 [bits 32]
 p_mode_start:
-       mov ax, SELECTOR_DATA
-   mov ds, ax
-   mov es, ax
-   mov ss, ax
-   mov esp,LOADER_STACK_TOP
-   mov ax, SELECTOR_VIDEO
-   mov gs, ax
+    mov ax, SELECTOR_DATA
+    mov ds, ax
 
-   ; 创建页目录及页表并初始化页内存位图
-   call setup_page
+    mov es, ax
+    mov ss, ax
 
-   ;要将描述符表地址及偏移量写入内存gdt_ptr,一会用新地址重新加载
-   sgdt [gdt_ptr]	      ; 存储到原来gdt所有的位置
+    mov esp, LOADER_STACK_TOP
+    mov ax, SELECTOR_VIDEO
+    mov gs, ax
 
-   ;将gdt描述符中视频段描述符中的段基址+0xc0000000
-   mov ebx, [gdt_ptr + 2]  
-   or dword [ebx + 0x18 + 4], 0xc0000000      ;视频段是第3个段描述符,每个描述符是8字节,故0x18。
-					      ;段描述符的高4字节的最高位是段基址的31~24位
+    ; 加载kernel
+    mov eax, KERNEL_START_SECTOR
+    mov ebx, KERNEL_BIN_BASE_ADDR
+    mov ecx, 200
 
-   ;将gdt的基址加上0xc0000000使其成为内核所在的高地址
-   add dword [gdt_ptr + 2], 0xc0000000
+    call rd_disk_m_32
+    
+    call setup_page
 
-   add esp, 0xc0000000        ; 将栈指针同样映射到内核地址
+    ; 保存gdt表
+    sgdt [gdt_ptr]
 
-   ; 把页目录地址赋给cr3
-   mov eax, PAGE_DIR_TABLE_POS
-   mov cr3, eax
+    ; 重新设置gdt描述符， 使虚拟地址指向内核的第一个页表
+    mov ebx, [gdt_ptr + 2]
+    or dword [ebx + 0x18 + 4], 0xc0000000
+    add dword [gdt_ptr + 2], 0xc0000000
+    
+    add esp, 0xc0000000
 
-   ; 打开cr0的pg位(第31位)
-   mov eax, cr0
-   or eax, 0x80000000
-   mov cr0, eax
+    ; 页目录基地址寄存器
+    mov eax, PAGE_DIR_TABLE_POS
+    mov cr3, eax
 
-   ;在开启分页后,用gdt新的地址重新加载
-   lgdt [gdt_ptr]             ; 重新加载
+    ; 打开分页
+    mov eax, cr0
+    or eax, 0x80000000
+    mov cr0, eax
 
-   mov byte [gs:160], 'V'     ;视频段段基址已经被更新,用字符v表示virtual addr
+    lgdt [gdt_ptr]
 
-   jmp $
+    ; 初始化kernel
+    jmp SELECTOR_CODE:enter_kernel
+
+    enter_kernel:
+        call kernel_init
+        mov esp, 0xc009f000
+        jmp KERNEL_ENTRY_POINT
+
+    jmp $
+
+; 创建页目录以及页表
 setup_page:
-;先把页目录占用的空间逐字节清0
-   mov ecx, 4096
-   mov esi, 0
-.clear_page_dir:
-   mov byte [PAGE_DIR_TABLE_POS + esi], 0
-   inc esi
-   loop .clear_page_dir
+    ; 页目录表占据4KB空间，清零之
+    mov ecx, 4096
+    mov esi, 0
+.clear_page_dir:   
+    mov byte [PAGE_DIR_TABLE_POS + esi], 0
+    inc esi
+    loop .clear_page_dir
 
-;开始创建页目录项(PDE)
-.create_pde:				     ; 创建Page Directory Entry
-   mov eax, PAGE_DIR_TABLE_POS
-   add eax, 0x1000 			     ; 此时eax为第一个页表的位置及属性
-   mov ebx, eax				     ; 此处为ebx赋值，是为.create_pte做准备，ebx为基址。
+; 创建页目录表(PDE)
+.create_pde:
+    mov eax, PAGE_DIR_TABLE_POS
+    ; 0x1000为4KB，加上页目录表起始地址便是第一个页表的地址
+    add eax, 0x1000
+    mov ebx, eax
 
-;   下面将页目录项0和0xc00都存为第一个页表的地址，
-;   一个页表可表示4MB内存,这样0xc03fffff以下的地址和0x003fffff以下的地址都指向相同的页表，
-;   这是为将地址映射为内核地址做准备
-   or eax, PG_US_U | PG_RW_W | PG_P	     ; 页目录项的属性RW和P位为1,US为1,表示用户属性,所有特权级别都可以访问.
-   mov [PAGE_DIR_TABLE_POS + 0x0], eax       ; 第1个目录项,在页目录表中的第1个目录项写入第一个页表的位置(0x101000)及属性(7)
-   mov [PAGE_DIR_TABLE_POS + 0xc00], eax     ; 一个页表项占用4字节,0xc00表示第768个页表占用的目录项,0xc00以上的目录项用于内核空间,
-					     ; 也就是页表的0xc0000000~0xffffffff共计1G属于内核,0x0~0xbfffffff共计3G属于用户进程.
-   sub eax, 0x1000
-   mov [PAGE_DIR_TABLE_POS + 4092], eax	     ; 使最后一个目录项指向页目录表自己的地址
+    ; 设置页目录项属性
+    or eax, PG_US_U | PG_RW_W | PG_P
+    ; 设置第一个页目录项
+    mov [PAGE_DIR_TABLE_POS], eax
+    ; 第768(内核空间的第一个)个页目录项，与第一个相同，这样第一个和768个都指向低端4MB空间
+    mov [PAGE_DIR_TABLE_POS + 0xc00], eax
+    ; 最后一个表项指向自己，用于访问页目录本身
+    sub eax, 0x1000
+    mov [PAGE_DIR_TABLE_POS + 4092], eax
 
-;下面创建页表项(PTE)
-   mov ecx, 256				     ; 1M低端内存 / 每页大小4k = 256
-   mov esi, 0
-   mov edx, PG_US_U | PG_RW_W | PG_P	     ; 属性为7,US=1,RW=1,P=1
-.create_pte:				     ; 创建Page Table Entry
-   mov [ebx+esi*4],edx			     ; 此时的ebx已经在上面通过eax赋值为0x101000,也就是第一个页表的地址 
-   add edx,4096
-   inc esi
-   loop .create_pte
+; 创建页表
+    mov ecx, 256
+    mov esi, 0
+    mov edx, PG_US_U | PG_RW_W | PG_P
+.create_pte:
+    mov [ebx + esi * 4], edx
+    add edx, 4096
+    inc esi
+    loop .create_pte
 
-;创建内核其它页表的PDE
-   mov eax, PAGE_DIR_TABLE_POS
-   add eax, 0x2000 		     ; 此时eax为第二个页表的位置
-   or eax, PG_US_U | PG_RW_W | PG_P  ; 页目录项的属性US,RW和P位都为1
-   mov ebx, PAGE_DIR_TABLE_POS
-   mov ecx, 254			     ; 范围为第769~1022的所有目录项数量
-   mov esi, 769
+; 创建内核的其它PDE
+    mov eax, PAGE_DIR_TABLE_POS
+    add eax, 0x2000
+    or eax, PG_US_U | PG_RW_W | PG_P
+    mov ebx, PAGE_DIR_TABLE_POS
+    mov ecx, 254
+    mov esi, 769
 .create_kernel_pde:
-   mov [ebx+esi*4], eax
-   inc esi
-   add eax, 0x1000
-   loop .create_kernel_pde
-   ret
+    mov [ebx + esi * 4], eax
+    inc esi
+    add eax, 0x1000
+    loop .create_kernel_pde
+    ret
+
+; 保护模式的硬盘读取函数
+rd_disk_m_32:
+
+    mov esi, eax
+    mov di, cx
+
+    mov dx, 0x1f2
+    mov al, cl
+    out dx, al
+
+    mov eax, esi
+    ; 保存LBA地址
+    mov dx, 0x1f3
+    out dx, al
+
+    mov cl, 8
+    shr eax, cl
+    mov dx, 0x1f4
+    out dx, al
+
+    shr eax, cl
+    mov dx, 0x1f5
+    out dx, al
+
+    shr eax, cl
+    and al, 0x0f
+    or al, 0xe0
+    mov dx, 0x1f6
+    out dx, al
+
+    mov dx, 0x1f7
+    mov al, 0x20
+    out dx, al
+
+.not_ready:
+    nop
+    in al, dx
+    and al, 0x88
+    cmp al, 0x08
+    jnz .not_ready
+
+    mov ax, di
+    mov dx, 256
+    mul dx
+    mov cx, ax
+    mov dx, 0x1f0
+
+.go_on_read:
+    in ax, dx
+    mov [ds:ebx], ax
+    add ebx, 2
+    loop .go_on_read
+    ret
+
+kernel_init:
+    xor eax, eax
+    xor ebx, ebx
+    xor ecx, ecx
+    xor edx, edx
+
+    mov dx, [KERNEL_BIN_BASE_ADDR + 42]
+    mov ebx, [KERNEL_BIN_BASE_ADDR + 28]
+
+    add ebx, KERNEL_BIN_BASE_ADDR
+    mov cx, [KERNEL_BIN_BASE_ADDR + 44]
+
+.each_segment:
+    cmp byte [ebx], PT_NULL
+    je .PTNULL
+
+    ; 准备mem_cpy参数
+    push dword [ebx + 16]
+    mov eax, [ebx + 4]
+    add eax, KERNEL_BIN_BASE_ADDR
+    push eax
+    push dword [ebx + 8]
+
+    call mem_cpy
+    add esp, 12
+
+.PTNULL:
+    add ebx, edx
+    loop .each_segment
+    ret
+
+mem_cpy:
+    cld
+    push ebp
+    mov ebp, esp
+    push ecx
+
+    mov edi, [ebp + 8]
+    mov esi, [ebp + 12]
+    mov ecx, [ebp + 16]
+    rep movsb
+
+    pop ecx
+    pop ebp
+    ret
